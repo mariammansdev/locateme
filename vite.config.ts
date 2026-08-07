aad71dab4c484d0f5b99702a7ee58c62b5ef1f5f
mariam <mariummansour19@gmail.com>
2026-08-07 17:33:19 +0300

    launch stunning locateMe - phase 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
+import { reactRouter } from "@react-router/dev/vite";
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/locateme/",
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(),reactRouter(),],
   resolve: {
     tsconfigPaths: true,
   },
